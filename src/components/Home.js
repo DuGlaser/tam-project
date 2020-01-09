04d5276781aeb969a29de6c7f15a38ccb142ad7a
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-09 17:37:34 +0900

    :sparkles:ADD Appbar to Home func
    ~

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { List, ListItem, ListItemText } from "@material-ui/core";
+import Appbar from "../components/Appbar";
 
 const Home = () => {
   return (
     <nav>
+      <Appbar />
       <List>
         <ListItem component={Link} to={"/"}>
-          <ListItemText>hoge</ListItemText>
+          <ListItemText>Home</ListItemText>
         </ListItem>
         <ListItem component={Link} to={"/about"}>
           <ListItemText>About</ListItemText>
