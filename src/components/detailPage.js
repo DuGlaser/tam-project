05cd820ace261af5941cf41178a1d1a2b050427a
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-09 16:53:30 +0900

    :sparkles:ADD Appbar to detailPage func
    ~

--- src/components/detailPage.js
+++ src/components/detailPage.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Appbar from "./Appbar";
 
 const DetailPage = props => {
   const { title } = props;
-  return <h1>{title}</h1>;
+  return (
+    <>
+      <Appbar />
+      <h1>{title}</h1>
+    </>
+  );
 };
 
 export default DetailPage;
