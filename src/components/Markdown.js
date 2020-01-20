7a8c7259516df1e9f56b410dcd803a6f875497f3
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-20 18:44:55 +0900

    :sparkles:ADD Markdown func
    ~

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,19 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Appbar from "./Appbar";
 import Editor from "./Editor";
 import Preview from "./Preview";
-import { Box } from "@material-ui/core";
+import { Box, Fab, LinearProgress } from "@material-ui/core";
+import { getNoteContent, UpdateNote } from "../query/query";
+import SaveIcon from "@material-ui/icons/Save";
 
-const Markdown = () => {
+const Markdown = props => {
+  const { isLoading, request, data } = props;
   const [html, setHtml] = useState("");
+
+  const ID = props.match.params.id;
+
+  useEffect(() => {
+    request(getNoteContent(ID));
+  }, []);
+  //
+  const saveMarkdown = () => {
+    request(UpdateNote(ID, html));
+  };
+
+  if (!isLoading) {
+    setHtml(data.note.content);
+  }
 
   return (
     <>
       <Appbar />
-      <Box display="flex">
-        <Editor html={html} setHtml={setHtml} />
-        <Preview html={html} />
-      </Box>
+      {isLoading ? (
+        <LinearProgress color="secondary" />
+      ) : (
+        <Box display="flex">
+          <Editor html={html} setHtml={setHtml} />
+          <Preview html={html} />
+          <Fab color="primary" aria-label="add">
+            <SaveIcon onClick={saveMarkdown} />
+          </Fab>
+        </Box>
+      )}
     </>
   );
 };
