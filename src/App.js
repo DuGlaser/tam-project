bfed6437944bc373420335d84c5b225c5225f846
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-20 18:51:20 +0900

    :ambulance:FIX path
    ~

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import { router, NoteRouter } from "./routes";
+import NoteRouter from "./routes";
 
 const App = () => {
   return <NoteRouter />;
