0c9fa4b16115b7819fedd2642c9260e43cfde74c
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-20 18:46:01 +0900

    :recycle:FIX initialState
    ~

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,26 +1,38 @@
 import { REQUEST, SUCCESS, FAIL } from "../actions/action";
 
-export const reducer = (state, action) => {
+export const initialState = {
+  data: {
+    notes: [],
+    note: []
+  },
+  inLoading: true
+};
+
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case REQUEST:
+      console.log(action);
       return {
         ...state,
         isLoading: true
       };
 
     case SUCCESS:
+      console.log(action);
       return {
         ...state,
-        // payload...
+        ...action.payload,
         isLoading: false
       };
 
     case FAIL:
+      console.log(action);
       return {
         ...state,
-        // error...
         isLoading: false
       };
     default:
+      return state;
   }
 };
+
